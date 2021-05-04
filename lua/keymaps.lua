9082663c1142830944bc0b2ae25145a43b96dca3
Rishabh Dutt <rishabh.dutt@citrix.com>
2021-05-04 17:45:29 +0530

    Fixed kymaps and settings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,34 +1,41 @@
-map = function (mode, newmap, oldmap, props) vim.api.nvim_set_keymap(mode, newmap, oldmap, props) end
+local utils = require('utils')
+map = utils.map 
 
 -- Set leader key
-map('n', '<Space>', '<NOP>', { noremap = true, silent = true })
+map('n', '<Space>', '<NOP>', { noremap = true })
 vim.g.mapleader = ' '
 
-map('n', '<Leader>h', ':set hlsearch!<CR>', { noremap = true, silent = true })
-map('n', '<Leader>n', ':set nu!<CR>', { noremap = true, silent = true })
+map('n', '<Leader>h', ':set hlsearch!<CR>', { noremap = true })
+map('n', '<Leader>n', ':set nu!<CR>', { noremap = true })
 
 -- nerd tree toggle
-map('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+map('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true })
 
 -- window movement
-map('n', '<C-h>', '<C-w>h', { silent = true })
-map('n', '<C-j>', '<C-w>j', { silent = true })
-map('n', '<C-k>', '<C-w>k', { silent = true })
-map('n', '<C-l>', '<C-w>l', { silent = true })
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
 -- tab indentation
-map('v', '<', '<gv', { noremap = true, silent = true })
-map('v', '>', '>gv', { noremap = true, silent = true })
+map('v', '<', '<gv', { noremap = true })
+map('v', '>', '>gv', { noremap = true })
 
 
 -- Tab switch buffer
-map('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true })
-map('n', '<S-TAB>', ':bprevious<CR>', { noremap = true, silent = true })
+map('n', '<TAB>', ':bnext<CR>', { noremap = true })
+map('n', '<S-TAB>', ':bprevious<CR>', { noremap = true })
 
 
 -- Buffer delete
-map('n', '<Leader>-', ':bdelete<CR>', { noremap = true, silent = true })
+map('n', '<Leader>-', ':bdelete<CR>', { noremap = true })
 
 -- Move selceted block of code
-map('x', 'K', ':move \'<-2<CR>gv-gv\'', { noremap = true, silent = true })
-map('x', 'J', ':move \'<+1<CR>gv-gv\'', { noremap = true, silent = true })
+map('x', 'K', ':move \'<-2<CR>gv-gv\'', { noremap = true })
+map('x', 'J', ':move \'<+1<CR>gv-gv\'', { noremap = true })
+
+
+-- Split windows
+map('n', '<Leader>s', ':sp<CR>', { noremap = true })
+map('n', '<Leader>v', ':vsp<CR>', { noremap = true })
+
